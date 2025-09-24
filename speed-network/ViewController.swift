caa9f2486bdb5697291265633c6396f71d065564
Miguel <jorquera1401@gmail.com>
2025-09-24 01:33:41 -0300

    Initial Commit

---
+++ speed-network/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  speed-network
+//
+//  Created by Miguel Jorquera on 24-09-25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ speed-networkTests/speed_networkTests.swift
@@ -0,0 +1,17 @@
+//
+//  speed_networkTests.swift
+//  speed-networkTests
+//
+//  Created by Miguel Jorquera on 24-09-25.
+//
+
+import Testing
+@testable import speed_network
+
+struct speed_networkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
